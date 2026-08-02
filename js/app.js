d051ef1cb8cfd5c3383802eb300332f86868f1b7
LockVision <hassanelmaayati@gmail.com>
2026-08-02 13:33:33 +0300

    added function startGame and event listener that takes the player to the next screen when clicked anywhere on screen

--- js/app.js
+++ js/app.js
@@ -2,15 +2,20 @@
 
 
 /*-------------- Functions -------------*/
+function startGame(){
+  startScreen.classList.add("hidden")
+  gameContainer.classList.remove("hidden")
 
+}
 
 /*---------- Variables (state) ---------*/
 
 
 /*----- Cached Element References  -----*/
-const startScreen = document.getElementById('start-screen')
+const startScreen = document.getElementById("start-screen")
 const gameContainer= document.getElementById("game-container")
 const sceneImage=document.getElementById("scene-image")
 const sceneText=document.getElementById("scene-text")
 const choicesContainer=document.getElementById("choices-container")
 /*----------- Event Listeners ----------*/
+startScreen.addEventListener("click",startGame)
