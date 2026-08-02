9fa05611138481b628f33b155b3da6da265a2762
LockVision <hassanelmaayati@gmail.com>
2026-08-02 14:37:05 +0300

    now the dialog changes with clicks using lineIndex

--- js/app.js
+++ js/app.js
@@ -5,10 +5,42 @@
 function startGame(){
   startScreen.classList.add("hidden")
   gameContainer.classList.remove("hidden")
+  render()
+
+}
+
+function render(){
+  choicesContainer.innerHTML=""
+  const scene= scenes[current]
+  sceneText.textContent=scene.dialog[lineIndex]
+  sceneImage.src=scene.image
+  if(lineIndex<scene.dialog.length-1){
+  }
+  else{
+    renderChoices(scene.choices)
+  }
+}
+function nextLineOfDialog(){
+  lineIndex++;
+  render()
+}
+
+function renderChoices(choices){
+    choices.forEach(choice => {
+    const btn = document.createElement('button');
+    btn.textContent = choice.label;
+    choicesContainer.appendChild(btn);
+    
+  });
 
 }
 
 /*---------- Variables (state) ---------*/
+// let, not const, because these values change as the game progresses
+
+let current="apartment"
+let lineIndex=0
+
 
 
 /*----- Cached Element References  -----*/
@@ -19,3 +51,4 @@ const sceneText=document.getElementById("scene-text")
 const choicesContainer=document.getElementById("choices-container")
 /*----------- Event Listeners ----------*/
 startScreen.addEventListener("click",startGame)
+gameContainer.addEventListener("click",nextLineOfDialog)
