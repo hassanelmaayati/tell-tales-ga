6baf67b9b6159d7db339515ef776fbd74645b199
LockVision <hassanelmaayati@gmail.com>
2026-08-02 13:14:49 +0300

    edited html structure and linked Ids to javascript (Cashed Element References)

--- js/app.js
+++ js/app.js
@@ -8,6 +8,9 @@
 
 
 /*----- Cached Element References  -----*/
-
-
+const startScreen = document.getElementById('start-screen')
+const gameContainer= document.getElementById("game-container")
+const sceneImage=document.getElementById("scene-image")
+const sceneText=document.getElementById("scene-text")
+const choicesContainer=document.getElementById("choices-container")
 /*----------- Event Listeners ----------*/
